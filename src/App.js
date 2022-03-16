eb1dcdb7c492f6aff2dc83e633c7ff173e2299df
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-03-16 07:07:46 +0000

    Add search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useMemo} from "react";
 import './App.css';
-import {pokemon$} from "./store";
+import {pokemonWithPower$} from "./store";
+
+
+const Search = () => {
+    const [search, setSearch] = useState('')
+    const [pokemon, setPokemon] = useState([])
+
+    useEffect(() => {
+        const sub = pokemonWithPower$.subscribe(setPokemon)
+        return sub.unsubscribe()
+    }, [])
+
+    const filteredPokemon = useMemo(() => {
+        return pokemon.filter(p => p.name.toLowerCase().includes(search.toLowerCase()))
+    }, [pokemon, search])
+
+    return (
+        <div>
+            <input type="text" value={search} onChange={e => setSearch(e.target.value)}/>
+            <div>
+                {filteredPokemon.map(p => <div key={p.id}>
+                    <strong>{p.name}</strong> - {p.power}
+                </div>)}
+            </div>
+        </div>
+    )
+}
 
 function App() {
     const [pokemons, setPokemons] = useState([])
-
     useEffect(() => {
-        const sub = pokemon$.subscribe(setPokemons)
+        const sub = pokemonWithPower$.subscribe(setPokemons)
+
         return () => sub.unsubscribe();
     }, [])
     return (
-        <div>
+        <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr'}}>
+            <Search/>
             <ul>
                 {pokemons.map(pokemon => <li key={pokemon.id}>{pokemon.name}</li>)}
             </ul>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,26 @@
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, map} from "rxjs";
 
 
 const pokemon$ = new BehaviorSubject([])
 
+const pokemonWithPower$ = pokemon$.pipe(
+    map(pokemon => {
+            return pokemon.map(p => {
+                return {
+                    ...p,
+                    power: p.hp + p.attack + p.defense + p.special_attack + p.special_defense + p.speed
+                }
+            })
+        }
+    )
+)
+
 fetch('/pokemon.json')
     .then(res => res.json())
-    .then(data => pokemon$.next(data))
+    .then(data => {
+        console.log(data)
+        return pokemon$.next(data)
+    })
 
 
-export {pokemon$}
+export {pokemon$,pokemonWithPower$}
